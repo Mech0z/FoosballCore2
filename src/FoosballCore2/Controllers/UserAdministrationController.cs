8139cfd9978f3a93a8b1b71e33b59c87a1c93763
Mads Skipper <madsskipper@gmail.com>
2016-12-16 23:16:15 +0100

    rename email method

--- src/FoosballCore2/Controllers/UserAdministrationController.cs
+++ src/FoosballCore2/Controllers/UserAdministrationController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Models;
 using Repository;
 
 namespace FoosballCore2.Controllers
@@ -6,15 +7,32 @@ namespace FoosballCore2.Controllers
     public class UserAdministrationController : Controller
     {
         private readonly IUserRepository _userRepository;
+        private readonly IMatchRepository _matchRepository;
 
-        public UserAdministrationController(IUserRepository userRepository)
+        public UserAdministrationController(IUserRepository userRepository, IMatchRepository matchRepository)
         {
             _userRepository = userRepository;
+            _matchRepository = matchRepository;
         }
         public IActionResult Index()
         {
             var users = _userRepository.GetUsers();
             return View(users);
+        }
+
+        public IActionResult ChangeEmail(string oldEmail, string newEmail)
+        {
+            var matches = _matchRepository.GetPlayerMatches(oldEmail);
+
+            foreach (Match match in matches)
+            {
+                var index = match.PlayerList.IndexOf(oldEmail);
+                match.PlayerList[index] = newEmail;
+                _matchRepository.SaveMatch(match);
+                //send update message
+            }
+
+            return View();
         }
 
         public IActionResult ChangeMailOfUser()
