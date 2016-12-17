1676a6c5ce00292ca565523059f10190f563a3cc
Mads Skipper <madsskipper@gmail.com>
2016-12-17 10:40:08 +0100

    signalr basic view

--- src/FoosballCore2/Hubs/MatchHub.cs
+++ src/FoosballCore2/Hubs/MatchHub.cs
@@ -6,5 +6,9 @@ namespace FoosballCore2.Hubs
     [HubName("match")]
     public class MatchHub : Hub
     {
+        public void SendMatchPlayed()
+        {
+            Clients.All.sayHello("sdf");
+        }
     }
 }
