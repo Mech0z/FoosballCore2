d2f7b7be193b2e78967fb9e09b7e2df4eba75866
Mads Skipper <madsskipper@gmail.com>
2017-02-21 21:41:23 +0100

    Gravatar on start match

--- src/Frontend/ClientApp/app/components/interfaces/interfaces.ts
+++ src/Frontend/ClientApp/app/components/interfaces/interfaces.ts
@@ -12,3 +12,28 @@ export interface LeaderboardViewEntry{
     Losses: number;
     UserName: string;
 }
+
+export interface Match {
+    TimeStampUtc: Date;
+    Points: number;
+    PlayerList: string[];
+    MatchResult: MatchResult;
+    Won: boolean;
+}
+
+export interface MatchResult {
+    Team1Score: number;
+    Team2Score: number;
+    Team1Won: boolean;
+}
+
+export interface PartnerResult {
+    Username: string;
+    UsersNormalWinrate: number;
+    Matches: number;
+    Wins: number;
+}
+
+export interface Player {
+    GravatarEmail: string;
+}
